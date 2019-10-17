85c00576dbcd78115172055efb216afa0a70fd22
jon <jonathan.wong110@gmail.com>
2019-10-17 14:41:15 -0400

    started working creating the products controller. Also edited the customer and store controller to have less routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product!, only [:show, :edit, :update]
+  
   def index
   end
 
@@ -6,17 +8,39 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(params)
   end
 
   def edit
   end
 
   def update
+    @product.update(product_params)
+    if @product.valid?
+      @product.save
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @product.destroy
+    redirect_to products_path
   end
+
+  private
+
+  def product_params
+    params.require(product).permit(:title, :price, :description, :category, :store_id)
+  end
+
+  def set_product!
+    @product = Product.find(params[:id])
+  end
+
 end
