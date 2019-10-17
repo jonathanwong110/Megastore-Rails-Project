decb0fbbf8e3afada2e24300798f75f47fcb82b7
jon <jonathan.wong110@gmail.com>
2019-10-17 18:55:06 -0400

    created a signup form for stores, put in some validations for the store model and worked on the create method for the store controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoresController < ApplicationController
   
   def index
-    @store = Store.all
+    @stores = Store.all
   end
   
   def new
@@ -10,6 +10,12 @@ class StoresController < ApplicationController
 
   def create
     @store = Store.new(store_params)
+    if @store.valid?
+      @store.save
+      redirect_to new_store_path
+    else
+      render 'new'
+    end
   end
 
   private
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,9 @@
 class Store < ApplicationRecord
+    has_secure_password
+
     has_many :products
+
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
+    validates :email, presence: true
+    validates :password, presence: true
 end
