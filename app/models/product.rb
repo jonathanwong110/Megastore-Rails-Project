4f9a698b7a3f477d83e48b2cfa6a5725794e5262
jon <jonathan.wong110@gmail.com>
2019-10-17 20:40:49 -0400

    added validations to the models, signup form for customers, and started the create method for the customer controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
     belongs_to :store
     belongs_to :cart
+
+    validates :title, presence: true
+    validates :price, presence: true
+    validates :description, presence: true
+    validates :category, presence: true
 end
