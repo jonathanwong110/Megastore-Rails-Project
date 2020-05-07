98d8e81ba67bba5a9b424d74329b4fe57a96cd12
jon <jonathan.wong110@gmail.com>
2020-05-07 03:22:00 -0400

    made some slight changes, made it so the cart shows quantity per items and no duplicates, working on showing a total price in the cart

--- app/controllers/cartproducts_controller.rb
+++ app/controllers/cartproducts_controller.rb
@@ -6,9 +6,9 @@ class CartproductsController < ApplicationController
     @cart = Cart.find_by(user_id: current_user.id)
     @cartproduct = CartProduct.find_by(product_id: @product.id, cart_id: @cart.id)
     if @cartproduct.destroy
+      redirect_to carts_path
       flash[:notice] = "*The product was removed from the cart successfully*"
     end
-    redirect_to carts_path
-    end
+  end
 
 end
