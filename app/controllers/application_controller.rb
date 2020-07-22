736a3858ed73e15ae62839dbd60e1d3db7b8d891
jonathanwong110 <jonathan.wong110@gmail.com>
2020-07-22 11:27:22 -0400

    made minor changes to navbar, made it so each product is presented like a card

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
   end
   
   def current_user
-    User.find(session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
   
   def require_login
