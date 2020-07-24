97b95b2a1924ac27833341746b69ad7ee2257024
jonathanwong110 <jonathan.wong110@gmail.com>
2020-07-23 23:56:51 -0400

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_192135) do
+ActiveRecord::Schema.define(version: 2019_10_25_041155) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "cart_id"
@@ -21,13 +21,6 @@ ActiveRecord::Schema.define(version: 2020_05_08_192135) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "product_reviews", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "review_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -54,13 +47,6 @@ ActiveRecord::Schema.define(version: 2020_05_08_192135) do
   create_table "user_products", force: :cascade do |t|
     t.integer "user_id"
     t.integer "product_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "user_reviews", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "review_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
